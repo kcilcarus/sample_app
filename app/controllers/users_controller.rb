803abe6f7768521299c5bdbca5e003cca9e51840
kcilcarus <kcilcarus@gmail.com>
2016-01-06 15:31:09 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
+    @microposts = @user.microposts.paginate(:page => params[:page], :per_page => 7)
   end
 
   def edit
